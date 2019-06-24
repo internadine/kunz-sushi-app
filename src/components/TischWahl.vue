a584240e05f708d6b7bc04a2a101cd32ad2ac51f
Nadine Wischmeier <nadine.wischmeier@outlook.com>
2019-06-24 17:06:50 +0200

    set Table at the beginning of order process

---
+++ src/components/TischWahl.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container text-center text-info">
+    <div class="mt-5">
+      <h2>Tisch auswählen</h2>
+    </div>
+    <div class="mt-5"> <button
+        v-for="(n, index) in 20"
+        :key="index"
+        type="button"
+        class="btn btn-outline-info m-1"
+        @click="setTable(n)"
+      >
+        {{n}}</button>
+      <button
+        @click="setTable(30)"
+        class="btn btn-outline-danger ml-1"
+      >30</button></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TischWahl",
+  methods: {
+    setTable(number) {
+      this.$store.dispatch("setTable", number);
+    }
+  }
+};
+</script>
